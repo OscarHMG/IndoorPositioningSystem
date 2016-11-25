39f53ba877fe54e2d7a8187ae66d29633734d92a
OscarHMG <oscar_mg7@hotmail.com>
2016-11-25 00:27:08 -0500

    First Example with Google Cloud. Can't tested, but have to work

--- IndoorPositioningSystem/app/src/main/java/com/oscarhmg/indoorpositioningsystem/WebViewMap.java
+++ IndoorPositioningSystem/app/src/main/java/com/oscarhmg/indoorpositioningsystem/WebViewMap.java
@@ -1,10 +1,14 @@
 package com.oscarhmg.indoorpositioningsystem;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Pair;
 import android.view.Menu;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+
+import cloudcontroller.EndPointAsyncTask;
 
 /**
  * Created by user on 17/11/2016.
@@ -15,6 +19,7 @@ public class WebViewMap extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.web_view_map);
+        //new EndPointAsyncTask().execute(new Pair<Context, String>(this,"Indoor Positioning System/ Materia Integradora"));
         WebView webView = (WebView)findViewById(R.id.webView);
         webView.setWebViewClient(new WebViewClient());
         webView.getSettings().setJavaScriptEnabled(true);
