249c0c064253171c00a4a3592177586cebaf8a45
Sergio <sergi_xavier01@hotmail.com>
2017-01-09 22:54:39 -0500

    add comments and clearing code

--- WebService/graphsMethods.py
+++ WebService/graphsMethods.py
@@ -2,12 +2,9 @@ import networkx as nx
 from networkx.readwrite import json_graph
 import json
 
-# get attributes
-#location = nx.get_node_attributes(G,'location')
-#path = json.dumps(location)
 
+#Method to read ther properties of the graph
 def read_json_file(filename):
-    #save_json()
     with open(filename) as f:
         js_graph = json.load(f)
     grafo = json_graph.node_link_graph(js_graph)
@@ -16,19 +13,20 @@ def read_json_file(filename):
     arcos = ("Edges: ", K)
     return "grafo: "+str(nodos) +str(arcos)
 
+#Method to load the graph on memory
 def read_json_file_graph(filename):
     with open(filename) as f:
         js_graph = json.load(f)
     grafo = json_graph.node_link_graph(js_graph)
     return grafo
 
+#Method to get the specific shortest path
 def shortest_path(puntosJson,filename):
     load_data = json.loads(puntosJson)
     #print load_data
     G = read_json_file_graph(filename)
     location = nx.get_node_attributes(G,'location')
     #print location
-
 
     inicial = [nodo for nodo in location.items() if nodo[1] == load_data['inicio']]
     print "------------------inicio:"
@@ -40,48 +38,32 @@ def shortest_path(puntosJson,filename):
 
     print nodo_final[0]
 
-    #resultado = nx.dijkstra_path(G,30,18)
     resultado = nx.dijkstra_path(G,nodo_inicial[0],nodo_final[0])
 
    #  lat , lng
-    latitud = nx.get_node_attributes(G,'lat')
     position = nx.get_node_attributes(G,'position')
 
-    lista_nodo = G.nodes(data=True)
+    lista_nodo = G.nodes(data=True) # get a list of nodes with attributes
 
-    
     path = []
 
-
                 
-   # for id_key in resultado:
-   #     for nodo in position.items():
-   #         if nodo[0]== id_key:
-   #             result_ver =  nodo[1]
-   #             print result_ver
+    # for id_key in resultado:
+    #     for nodo in position.items():
+    #         if nodo[0]== id_key:
+    #            result_ver =  nodo
+    #            print result_ver
     #            path.append(result_ver)
     for id_key in resultado:
-        result_ver =  [nodo[1] for nodo in position.items() if nodo[0]== id_key]
-        path.append(result_ver)
+       result_ver =  [nodo for nodo in position.items() if nodo[0]== id_key]
+       path.append(result_ver)
     
-       
-
-   #shortest_path_ver={}
-   #for nodo in path:
-    #  shortest_path_ver= 
-    
-
-      
-
+          
     print path
 
-
-
-
-    #resultado = nx.dijkstra_path(grafo,1,18)
+    #resultado = nx.dijkstra_path(grafo,1,18)  path not found
     return json.dumps(path)
-#nodo = [nodo  for nodo in location.items() if nodo[1]=='salaespera']
-#[(0, 'salaespera')]
+
 
 #search(myDict, 'Mary')
 def search(values, searchFor):
